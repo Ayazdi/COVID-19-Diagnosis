0b331b0f9a0478305cb7841d85cb86cc3ba07745
Amirali <amirali.yazdi@yahoo.com>
2020-06-04 10:23:22 +0200

    update flask

--- application.py
+++ application.py
@@ -21,7 +21,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 @app.route('/index')
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -39,13 +38,21 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             prediction = ct_scan_diagnosis(file)
 
             return render_template('recommender.html',
-                                   result_html=prediction)
+                                   result_html=prediction,
+                                   ct_scan_image=full_filename)
     return render_template('index.html')
 
 
+# @app.route('/show/<filename>')
+# def uploaded_file(filename):
+#     filename = 'http://127.0.0.1:5000/uploads/' + filename
+#     return render_template('recommender.html', filename=filename)
+#
+#
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
